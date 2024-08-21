a15bbc430d74a27b0568198f701186f216a003c4
Ati <atialikhani8@gmail.com>
2024-08-22 02:22:43 +0330

    add remove_todo functions

--- web.py
+++ web.py
@@ -9,12 +9,21 @@ def add_todo():
     functions.set_todos(todos)
 
 
+def remove_todo(todo):
+    todos.remove(todo)
+    functions.set_todos(todos)
+    st.rerun()
+
+
+
 st.title("My Todo App")
 st.subheader("This is my todo app")
 st.write("This app to increase your productivity.")
 
 for index, todo in enumerate(todos):
-    st.checkbox(todo, key=f"checkbox_{index}")
+    checkbox=st.checkbox(todo, key=f"checkbox_{index}")
+    if checkbox:
+        remove_todo(todo)
 
 st.text_input(label="", placeholder="Add new todo...",
               on_change=add_todo, key="new_todo")
